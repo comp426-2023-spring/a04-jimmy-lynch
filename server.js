35f2ffdad88c698cd8f1a65f7e97e295874de6d5
jdlynch13 <jimmyl@brimmatech.com>
2023-03-29 21:51:39 -0400

    attempt 1

--- server.js
+++ server.js
@@ -17,10 +17,31 @@ app.get('/app', (req, res) => {
 })
 
 //(4) check endpoint at /app/rps
+app.get('/app/rps/', (req, res) => {
+    res.status(200).send(JSON.stringify(rps()));
+})
 
 //(5) /app/rpsls endpoint
+app.get('/app/rpsls/', (req, res) => {
+    res.status(200).send(JSON.stringify(rpsls()));
+})
 
 //(6) /app/rps/play endpoint
+app.get('/app/rps/play/', (req, res) => {
+    res.status(200).send(JSON.stringify(req.query.shot));
+})
+
+app.post('/app/rps/play/', (req, res) => {
+    res.status(200).send(JSON.stringify(req.body.shot));
+})
+
+app.get('/app/rpsls/play/', (req, res) => {
+    res.status(200).send(JSON.stringify(req.query.shot));
+})
+
+app.post('/app/rpsls/play/', (req, res) => {
+    res.status(200).send(JSON.stringify(req.body.shot));
+})
 
 //(7) /app/rpsls/play endpoint
 
