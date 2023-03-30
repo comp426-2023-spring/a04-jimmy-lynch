d6f031ab6662daaf69fc0805bde5ca4f5daf4fc3
jdlynch13 <jimmyl@brimmatech.com>
2023-03-29 21:54:01 -0400

    forgot to call functions lol

--- server.js
+++ server.js
@@ -9,40 +9,45 @@ app.use(express.urlencoded({ extended: true}));
 const args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
 
-//(2) default endpoint)
+// default endpoint)
 
-//(3) Check endpoint @ /app
+// Check endpoint @ /app
 app.get('/app', (req, res) => {
     res.status(200).send("200 OK");
 })
 
-//(4) check endpoint at /app/rps
+// check endpoint at /app/rps
 app.get('/app/rps/', (req, res) => {
     res.status(200).send(JSON.stringify(rps()));
 })
 
-//(5) /app/rpsls endpoint
+// /app/rpsls endpoint
 app.get('/app/rpsls/', (req, res) => {
     res.status(200).send(JSON.stringify(rpsls()));
 })
 
-//(6) /app/rps/play endpoint
-app.get('/app/rps/play/', (req, res) => {
-    res.status(200).send(JSON.stringify(req.query.shot));
+// /app/rps/play endpoint (bodies & params)
+app.get('/app/rps/play/', (req, res) => { //rps URLEncoded
+    res.status(200).send(JSON.stringify(rps(req.query.shot)));
 })
 
-app.post('/app/rps/play/', (req, res) => {
-    res.status(200).send(JSON.stringify(req.body.shot));
+app.post('/app/rps/play/', (req, res) => { //rps JSON
+    res.status(200).send(JSON.stringify(rps(req.body.shot)));
 })
 
-app.get('/app/rpsls/play/', (req, res) => {
-    res.status(200).send(JSON.stringify(req.query.shot));
+app.get('/app/rps/play/:shot', (req, res) => { //rps shot params
+    res.status(200).send(JSON.stringify(rps(req.params.shot)));
 })
 
-app.post('/app/rpsls/play/', (req, res) => {
-    res.status(200).send(JSON.stringify(req.body.shot));
+// /app/rpsls/play endpoint
+app.get('/app/rpsls/play/', (req, res) => { //rpsls URLEncoded
+    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
 })
 
-//(7) /app/rpsls/play endpoint
+app.post('/app/rpsls/play/', (req, res) => { //rpsls JSON
+    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+})
 
-//(8) /app/rpsls/play/(rock...) endpoint
+app.get('/app/rpsls/play/:shot', (req, res) => { //rpsls shot params
+    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+})
