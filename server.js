7445225dc26bbf01c1f8ea922fba5572b65779b9
jdlynch13 <jimmyl@brimmatech.com>
2023-03-29 21:57:47 -0400

    attempt #3

--- server.js
+++ server.js
@@ -10,6 +10,13 @@ const args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
 
 // default endpoint)
+app.get("/app/*", (req, res) => {
+    res.status(404).send("404 NOT FOUND");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${port}`);
+})
 
 // Check endpoint @ /app
 app.get('/app', (req, res) => {
@@ -28,26 +35,26 @@ app.get('/app/rpsls/', (req, res) => {
 
 // /app/rps/play endpoint (bodies & params)
 app.get('/app/rps/play/', (req, res) => { //rps URLEncoded
-    res.status(200).send(JSON.stringify(rps(req.query.shot)));
+    res.status(200).send(JSON.stringify(rps(req.query.move)));
 })
 
 app.post('/app/rps/play/', (req, res) => { //rps JSON
-    res.status(200).send(JSON.stringify(rps(req.body.shot)));
+    res.status(200).send(JSON.stringify(rps(req.body.move)));
 })
 
-app.get('/app/rps/play/:shot', (req, res) => { //rps shot params
-    res.status(200).send(JSON.stringify(rps(req.params.shot)));
+app.get('/app/rps/play/:move', (req, res) => { //rps shot params
+    res.status(200).send(JSON.stringify(rps(req.params.move)));
 })
 
 // /app/rpsls/play endpoint
 app.get('/app/rpsls/play/', (req, res) => { //rpsls URLEncoded
-    res.status(200).send(JSON.stringify(rpsls(req.query.shot)));
+    res.status(200).send(JSON.stringify(rpsls(req.query.move)));
 })
 
 app.post('/app/rpsls/play/', (req, res) => { //rpsls JSON
-    res.status(200).send(JSON.stringify(rpsls(req.body.shot)));
+    res.status(200).send(JSON.stringify(rpsls(req.body.move)));
 })
 
-app.get('/app/rpsls/play/:shot', (req, res) => { //rpsls shot params
-    res.status(200).send(JSON.stringify(rpsls(req.params.shot)));
+app.get('/app/rpsls/play/:move', (req, res) => { //rpsls shot params
+    res.status(200).send(JSON.stringify(rpsls(req.params.move)));
 })
