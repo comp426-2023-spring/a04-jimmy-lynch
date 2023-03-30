a0014766bd4f13106f00ce2af6d7cc880be33c13
jdlynch13 <jimmyl@brimmatech.com>
2023-03-29 21:45:03 -0400

    updated package.json

--- server.js
+++ server.js
@@ -9,6 +9,19 @@ app.use(express.urlencoded({ extended: true}));
 const args = minimist(process.argv.slice(2));
 const port = args.port || 5000;
 
+//(2) default endpoint)
+
+//(3) Check endpoint @ /app
 app.get('/app', (req, res) => {
     res.status(200).send("200 OK");
 })
+
+//(4) check endpoint at /app/rps
+
+//(5) /app/rpsls endpoint
+
+//(6) /app/rps/play endpoint
+
+//(7) /app/rpsls/play endpoint
+
+//(8) /app/rpsls/play/(rock...) endpoint
